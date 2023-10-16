0ed3b7bbf323d99c8ebf4c111bc9795188d5fe9c
RijuE <136147920+RijuE@users.noreply.github.com>
2023-10-16 22:17:58 +0530

    Active Script updates
    Active Script updated.

---
+++ js/erijuonlineschool.js
@@ -0,0 +1,24 @@
+// E Riju Online School JavaScript Document
+$(function () {
+  var path = window.location.pathname;
+  var page = path.split("/").pop();
+  //console.log( page );
+  if (page != "") {
+	$(".nav-item .nav-link").each(function () {
+	  var linkname = $(this).attr("href");
+	  if (page == linkname) {
+		$(this).parent().addClass("active");
+		//console.log(linkname);
+	  }
+	});
+	$(".dropdown-menu .dropdown-item").each(function () {
+	  var sublinkname = $(this).attr("href");
+	  if (page == sublinkname) {
+		$(this).addClass("active");
+		$(this).parent().parent().addClass("active");
+		//console.log(sublinkname);
+	  }
+	});
+	var linklength = $(".nav-item .nav-link").length;
+  }
+});
